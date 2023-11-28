153465ad56a013fe595f4cd60896c656c2fb9e69
Nicolas Hovart <hovart.nicolas@gmail.com>
2023-11-28 19:15:45 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from './components/Button.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <Button color='blue' text='bleue' />
+        <Button color='red' text='rouge' />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+
+const Button = (props) => {
+    return (
+        <button style={{ backgroundColor: props.color }}>
+            Pillule {props.text}
+        </button>
+    )
+}
+
+
+export default Button
+
