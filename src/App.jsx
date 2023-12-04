4b42db59052f902f5c42296107a0621cf438ee9f
Nicolas Hovart <hovart.nicolas@gmail.com>
2023-12-04 17:37:14 +0100

    Add a state counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css'
 import Button from './components/Button.jsx'
+import { useState } from 'react' // TODO: check signed import
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  // fonction qui est déclenchée lorque on clique sur le bouton rouge
+  const handleClick = () => {
+    console.log("on incrémente le compteur")
+    setCounter((counter) => counter + 1) // on lui indique comment calculer la nouvelle valeur avec la précédente
+  }
 
   return (
     <>
@@ -9,6 +17,8 @@ function App() {
         <Button color='blue' text='bleue' />
         <Button color='red' text='rouge' />
       </div>
+      <button onClick={handleClick}>Click me </button>
+      <div>Vous avez cliqué {counter} fois ! </div>
     </>
   )
 }
